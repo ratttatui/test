fd17ead07dd79979a89ae3878f2bb590682ae7a5
ratttatui <jeanxarj@gmail.com>
2026-07-24 21:10:22 +0700

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,28 @@
+
+# print("circle calculator: ", end="")
+# a = input()
+# print("This is data type of a before casting:",type(a))
+# # this line casting a from string into integer
+# a = int(a)
+# print("This is data type of a after casting:",type(a))
+# # this line try to calculate a circle area using formaull Pi(r)(r)
+# print(3.14*(a**2))
+
+# b = input()
+
+# print("_"*20,"\n")
+# print("".ljust(3),"receipt: "+b)
+
+# print("Hello".ljust(10),"Mel")
+
+a = input()
+b = input()
+c =int(a)
+d =int(b)
+
+if c > d:
+    print(c)
+elif c == d:
+    print("equal")
+else :
+    print(d)
